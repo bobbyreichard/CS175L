c382db7c32030a3c2e8888819611817803c6bf0f
bobbyreichard <86379903+bobbyreichard@users.noreply.github.com>
2021-12-01 13:39:07 -0500

    Add files via upload

---
+++ matplotlib.py
@@ -0,0 +1,23 @@
+#matplotlib
+
+import matplotlib
+
+#main
+def main():
+    file_name = open('expenses.txt', 'r')
+    expenses = file_name.readlines()
+    file_name.close
+
+    for i in range(len(expenses)):
+        expenses[i] = expenses[i].rstrip('\n')
+
+    slice_labels = ['Rent', 'Gas', 'Food', 'Clothing', 'Car Payment', 'Misc']
+
+    plt.pie(expenses, labels = slice_labels)
+
+    plt.title('Monthly Expenses')
+
+    plt.show()
+
+if __name__=='__main__':
+    main()
